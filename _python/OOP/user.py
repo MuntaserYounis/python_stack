fa19bf731b760eb75e71d38986659339c5c0306b
MuntaserYounis <muntaserdagher@gmail.com>
2023-05-11 01:38:08 +0300

    user

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -14,18 +14,18 @@ class User:
 Muntaser = User('Muntaser Younis','muntaserdagher@gmail.com')
 Shatha = User('Shat-ha Bast','shatha@bast.com')
 Khalid = User('Khalid Khader','khalid@khader.com')
-
+# Have the first user make 3 deposits and 1 withdrawal and then display their balance
 Muntaser.make_deposit(100)
 Muntaser.make_deposit(200)
 Muntaser.make_deposit(250)
 Muntaser.display_user_balance()
-
+# Have the second user make 2 deposits and 2 withdrawals and then display their balance
 Shatha.make_deposit(1000)
 Shatha.make_deposit(10000)
 Shatha.make_withdrawal(200)
 Shatha.make_withdrawal(5000)
 Shatha.display_user_balance()
-
+# Have the third user make 1 deposits and 3 withdrawals and then display their balance
 Khalid.make_deposit(1000)
 Khalid.make_withdrawal(500)
 Khalid.make_withdrawal(400)
