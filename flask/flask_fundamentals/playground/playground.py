d9b53d62d92eec6ec92955772a7aa513036c9b1c
MuntaserYounis <muntaserdagher@gmail.com>
2023-05-21 12:36:24 +0300

    playground

---
+++ flask/flask_fundamentals/playground/playground.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/play') #the '/' after play (question)
+def index():
+    return render_template('index.html')
+
+@app.route('/play/<x>')
+def display(x):
+    return render_template('play.html',repeats = int(x))
+
+@app.route('/play/<x>/<color>')
+def display_color(x,color):
+    return render_template('play.html',repeats = int(x),my_color = color)
+if __name__ == '__main__':
+    app.run(debug =True) #still shwoing errors
+    
