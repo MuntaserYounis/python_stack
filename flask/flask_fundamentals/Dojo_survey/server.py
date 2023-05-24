f7f779c51814b2909c0d3341c57c055fb7b8e03a
MuntaserYounis <muntaserdagher@gmail.com>
2023-05-24 23:20:02 +0300

    dojo_survey

---
+++ flask/flask_fundamentals/Dojo_survey/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/result', methods = ["POST"])
+def process():
+    return render_template('output.html',name = request.form['name'],loc = request.form['location'],fav = request.form['language'],comment = request.form['comment'])
+if __name__ == '__main__':
+    app.run(debug=True)
