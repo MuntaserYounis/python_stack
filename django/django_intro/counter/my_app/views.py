e206d1d8a1b6381f618af4984c33566572d3dbf6
MuntaserYounis <muntaserdagher@gmail.com>
2023-06-08 16:19:04 +0300

    commit

--- django/django_intro/counter/my_app/views.py
+++ django/django_intro/counter/my_app/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render,redirect
 
 def index(request):
-    # counter = request.session.get('counter',0)+1 #another way of solving
-    # request.session['counter'] = counter
-    if 'counter' in request.session:
-        request.session['counter'] +=1
-    else:
-        request.session['counter'] = 0
-
-    counter = request.session['counter']
-    context = {
-        'muntaser' : counter
-    }
+    counter = request.session.get('counter',0)+1 #explain in more details 
+    request.session['counter'] = counter
+    
     print(counter)
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 def delete(request):
     del request.session['counter']
     return redirect('/')
-
-
-    
