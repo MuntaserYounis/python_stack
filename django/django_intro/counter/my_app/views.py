724bd62b005cf853420cef9e189d8d48544ef230
MuntaserYounis <muntaserdagher@gmail.com>
2023-06-08 15:03:34 +0300

    counter

---
+++ django/django_intro/counter/my_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,redirect
+
+def index(request):
+    counter = request.session.get('counter',0)+1 #explain in more details 
+    request.session['counter'] = counter
+    print(counter)
+    return render(request, 'index.html')
+
+def delete(request):
+    del request.session['counter']
+    return redirect('/')
