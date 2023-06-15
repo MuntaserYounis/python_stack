5f2c7a89c6c41c6f608a5fb9e3d689f5d259384b
MuntaserYounis <muntaserdagher@gmail.com>
2023-06-15 23:57:23 +0300

    half of books_authors

--- django/django_orm/books_authors_proj/books_authors_app/urls.py
+++ django/django_orm/books_authors_proj/books_authors_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.index),
     path('authors',views.authors),
     path('add_book',views.add_book),
-    path('books/<id>',views.view),
+    path('books/<int:id>',views.view,name = 'view'),
+    path('adding_author',views.adding_author),
 ]
--- django/django_orm/books_authors_proj/books_authors_app/views.py
+++ django/django_orm/books_authors_proj/books_authors_app/views.py
@@ -12,7 +12,17 @@ def add_book(request):
     Book.objects.create(title = request.POST['book_title'],desc = request.POST['book_desc'])
     return redirect('/')
 
-def view(request):
-    context = {'books':Book.objects.all()}
-    return render(request,'')
+def view(request,id):
+    book = Book.objects.get(id = id)
+    authors = book.authors.all()
+    all_authors = Author.objects.all()
+    context = {'book':book, 
+                'authors' : authors,
+                'all_authors':all_authors}
+    return render(request,'books.html',context)
 
+def adding_author(request):
+    author = Author.objects.get(id = request.POST['add_author'])
+    this_book = Book.objects.get(id = request.POST['book_id'])
+    author.books.add(this_book)
+    return redirect('view',id=this_book.id)
